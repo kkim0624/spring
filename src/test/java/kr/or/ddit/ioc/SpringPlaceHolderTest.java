30c0d6bdadaf1765f83d0194963b7c2b2a027fc8
KBH <bumpercar93@gmail.com>
2019-06-20 12:34:21 +0900

    spring ioc placeholder

---
+++ src/test/java/kr/or/ddit/ioc/SpringPlaceHolderTest.java
@@ -0,0 +1,41 @@
+package kr.or.ddit.ioc;
+
+import static org.junit.Assert.*;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import kr.or.ddit.ioc.placeholder.DbInfo;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:kr/or/ddit/ioc/application-ioc-placeholder.xml")
+public class SpringPlaceHolderTest {
+
+	@Resource(name = "dbInfo")
+	DbInfo dbInfo;
+	
+	/**
+	 * 
+	* Method : springPlaceHolderTest
+	* 작성자 : PC06
+	* 변경이력 :
+	* Method 설명 : spring placeholder test
+	 */
+	@Test
+	public void placeHolderTest() {
+		/***Given***/
+
+		/***When***/
+
+		/***Then***/
+		assertEquals("oracle.jdbc.driver.OracleDriver", dbInfo.getDriver());
+		assertEquals("jdbc:oracle:thin:@localhost:1521:xe", dbInfo.getUrl());
+		assertEquals("PC06", dbInfo.getUsername());
+		assertEquals("java", dbInfo.getPassword());
+	}
+
+}
